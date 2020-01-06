24a55a41ee61e8bb723d960b1c3fc61c37f5d471
Benicio Daniel Barreto <beni@MacBook-Pro-de-Benicio.local>
2020-01-06 00:09:21 -0300

    Implementa transferir

--- src/Conta.php
+++ src/Conta.php
@@ -2,9 +2,9 @@
 
 class Conta
 {
-    private string $cpfTitular;
-    private string $nomeTitular;
-    private float $saldo;
+    private $cpfTitular;
+    private $nomeTitular;
+    private $saldo = 0;
 
 
     public function __construct(string $cpfTitular, string $nomeTitular, float $saldo)
@@ -15,22 +15,33 @@ class Conta
     }
 
 
-    public function sacar(float $valorASacar)
+    public function sacar(float $valorASacar) : void
     {
         if ($valorASacar > $this->saldo){
             echo "Saldo indisponível";
-        } else {
-            $this->saldo -= $valorASacar;
+            return;
         }
+        $this->saldo -= $valorASacar;
     }
 
 
     public function depositar(float $valorADepositar) : void
     {
-        if ($valorADepositar > 0) {
-            $this->saldo += $valorADepositar;
-        } else {
+        if ($valorADepositar <= 0) {
             echo "Valor precisa ser positivo";
+            return;
         }
+        $this->saldo += $valorADepositar;
+    }
+
+
+    public function transferir(float $valorATransferir, Conta $contaDestino) : void
+    {
+        if ($valorATransferir > $this->saldo) {
+            echo "Saldo indisponível!";
+            return;
+        }
+            $this->sacar($valorATransferir);
+            $contaDestino->depositar($valorATransferir);
     }
 }

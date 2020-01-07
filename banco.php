ce77491f46c2b5b61c282b1be09ccb48b915297b
Benicio Daniel Barreto <beni@MacBook-Pro-de-Benicio.local>
2020-01-06 23:45:13 -0300

    Implementa classe Endereco

--- banco.php
+++ banco.php
@@ -3,10 +3,16 @@
 require_once 'src/Conta.php';
 require_once 'src/Titular.php';
 require_once 'src/Cpf.php';
+require_once 'src/Endereco.php';
 
-$benicio = new Titular(new Cpf('065.676.599-20'), 'Jose da Silva');
+$endereco = new Endereco('Curitiba', 'bairro qualquer', 'rua minha', '123b');
+
+$benicio = new Titular(new Cpf('065.676.599-20'), 'Jose da Silva', $endereco);
+
 $primeiraConta = new Conta($benicio);
 
 echo $primeiraConta->recuperaSaldo() . PHP_EOL;
 
 echo $primeiraConta->recuperaNomeTitular();
+
+var_dump($primeiraConta);
--- src/Titular.php
+++ src/Titular.php
@@ -5,12 +5,14 @@ class Titular
 {
     private $cpf;
     private $nome;
+    private $endereco;
 
-    public function __construct(Cpf $cpf, string $nome)
+    public function __construct(Cpf $cpf, string $nome, Endereco $endereco)
     {
         $this->cpf = $cpf;
         $this->validaNomeTitular($nome);
         $this->nome = $nome;
+        $this->endereco = $endereco;
     }
 
 
@@ -33,5 +35,11 @@ class Titular
             echo "O nome do titular deve possuir pelo menos 5 caracteres";
             exit();
         }
+    }
+
+
+    public function recuperaEndereco() : Endereco
+    {
+        return $this->endereco;
     }
 }
